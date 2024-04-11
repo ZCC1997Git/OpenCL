cd0565e1e0b8c4a1e34ae972e992e88307e9004d
zhangchenchen <zhangchenchen1997@outlook.com>
2024-04-11 19:15:56 +0800

    On branch master Changes to be committed: 	new file:   Convolution/.vscode/c_cpp_properties.json 	new file:   Convolution/.vscode/settings.json 	new file:   Convolution/Makefile 	new file:   Convolution/bin/ImageRotate 	new file:   Convolution/bin/ImageRotate_cpu 	new file:   Convolution/include/GetDeviceInfo.hpp 	new file:   Convolution/include/GetPlatformInfo.hpp 	new file:   Convolution/include/opencl.hpp 	new file:   Convolution/include/opencl_step.hpp 	new file:   Convolution/opencl/convolution.cl 	new file:   Convolution/src/BuildProgram.cpp 	new file:   Convolution/src/CreateCommadQueue.cpp 	new file:   Convolution/src/CreateContext.cpp 	new file:   Convolution/src/CreateKernel.cpp 	new file:   Convolution/src/CreateProgram.cpp 	new file:   Convolution/src/GetDeviceInfo.cpp 	new file:   Convolution/src/GetPlatform.cpp 	new file:   Convolution/src/GetPlatformInfo.cpp 	new file:   Convolution/src/ReadKernelSource.cpp 	new file:   Convolution/src/main.cpp 	new file:   event/.vscode/settings.json 	new file:   event/bin/Event 	new file:   event/obj/BuildProgram.o 	new file:   event/obj/CreateCommadQueue.o 	new file:   event/obj/CreateContext.o 	new file:   event/obj/CreateKernel.o 	new file:   event/obj/CreateProgram.o 	new file:   event/obj/GetDeviceInfo.o 	new file:   event/obj/GetPlatform.o 	new file:   event/obj/GetPlatformInfo.o 	new file:   event/obj/ReadKernelSource.o 	new file:   event/obj/main.o

---
+++ Convolution/src/CreateKernel.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <opencl.hpp>
+#include <string>
+
+cl_kernel CreateKernel(cl_program program, std::string kernel_name) {
+    cl_int errNum;
+    cl_kernel kernel = clCreateKernel(program, kernel_name.data(), &errNum);
+    if (errNum != CL_SUCCESS) {
+        std::cerr << "Failed to create kernel: " << kernel_name << std::endl;
+        return nullptr;
+    }
+    return kernel;
+}
